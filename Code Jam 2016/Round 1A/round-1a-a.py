9a2bfabd46a6ef1508077968e93ea563d43e5df0
George Sakkas <george.p.sakkas@gmail.com>
2018-04-18 20:09:43 +0300

    Added Code Jam 2016 Round 1A A problem solution

--- Code Jam 2016/Round 1A/round-1a-a.py
+++ Code Jam 2016/Round 1A/round-1a-a.py
@@ -1,29 +1,20 @@
-fin = open('C-tiny-practice.in', 'r')
-T = fin.readline().strip()
-T = int(T)
-fout = open('C-tiny-1a.out', 'w')
+def read_int():
+    return int(raw_input())
 
-for t in range(1, T + 1):
-	N = fin.readline().strip()
-	N = int(N)
-	line = fin.readline().split()
-	maxed = 0
-	for idx in range(N):
-		bffs = {idx: int(line[idx])-1}
-		j = bffs[idx]
-		count = 1
-		prev = idx
-		while j not in bffs.keys() and int(line[j])-1 not in bffs.values():
-			bffs[j] = int(line[j])-1
-			prev = j
-			j = bffs[prev]
-			count += 1
-		if j in bffs.keys() and j == idx and maxed < count:
-			maxed = count
-		elif int(line[j])-1 in bffs.values() and int(line[j])-1 == prev and maxed < count:
-			maxed = count+1
 
-	fout.write('Case #{0}: {1}\n'.format(t, maxed))
+def solve(s):
+    result = s[0]
+    for sub in s[1:]:
+        if sub < result[0]:
+            result += sub
+        else:
+            result = sub + result
+    return result
 
-fin.close()
-fout.close()
+
+if __name__ == "__main__":
+    t = read_int()
+    for test in xrange(1, t + 1):
+        s = raw_input()
+        print "Case #{}: {}".format(test, solve(s))
+    exit(0)
