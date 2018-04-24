ca3059d688befa67573b173526a3f6c45c04522a
George Sakkas <george.p.sakkas@gmail.com>
2018-04-24 21:15:24 +0300

    Cleaner code in old solutions

--- Code Jam 2016/Round 1B/round-1b-b.py
+++ Code Jam 2016/Round 1B/round-1b-b.py
@@ -5,74 +5,75 @@ fout = open('B-small-1b-practice.out', 'w')
 
 
 for t in range(1, T + 1):
-	line = list(map(list, fin.readline().split()))
-	print(line)
-	for i in range(len(line[0])):
-		if line[0][i] == '?' and line[1][i] == '?':
-			if i + 2 <= len(line[0]):
-				if line[0][i+1] != '?' and line[1][i+1] != '?':
-					if int(line[0][i+1]) == int(line[1][i+1]):
-						line[0][i] = '0'
-						line[1][i] = '0'
-					elif abs(int(line[0][i+1]) - int(line[1][i+1])) < 5:
-						line[0][i] = '0'
-						line[1][i] = '0'
-						if int(line[0][i+1]) < int(line[1][i+1]):
-							for j in range(i, len(line[0])):
-								if line[0][j] == '?':
-									line[0][j] = '9'
-								if line[1][j] == '?':
-									line[1][j] = '0'
-							break
-						else:
-							for j in range(i, len(line[0])):
-								if line[0][j] == '?':
-									line[0][j] = '0'
-								if line[1][j] == '?':
-									line[1][j] = '9'
-							break
-					elif abs(int(line[0][i+1]) - int(line[1][i+1])) > 5:
-						if int(line[0][i+1]) < int(line[1][i+1]):
-							line[0][i] = '1'
-							line[1][i] = '0'
-							for j in range(i, len(line[0])):
-								if line[0][j] == '?':
-									line[0][j] = '0'
-								if line[1][j] == '?':
-									line[1][j] = '9'
-							break
-						else:
-							line[0][i] = '0'
-							line[1][i] = '1'
-							for j in range(i, len(line[0])):
-								if line[0][j] == '?':
-									line[0][j] = '9'
-								if line[1][j] == '?':
-									line[1][j] = '0'
-							break
-					else:
-						if int(line[0][i+1]) < int(line[1][i+1]):
-							for j in range(i, len(line[0])):
-								if line[0][j] != '?':
-									line[0][j] = '0'
-								if line[1][j] != '?':
-									line[1][j] = '9'
-							break
-						else:
-							for j in range(i, len(line[0])):
-								if line[0][j] == '?':
-									line[0][j] = '9'
-								if line[1][j] == '?':
-									line[1][j] = '0'
-							break
-			else:
-				line[0][i] = '0'
-				line[1][i] = '0'
-		elif line[0][i] == '?' or line[1][i] == '?':
-			line[0][i] = '0'
-			line[1][i] = '0'
+    line = list(map(list, fin.readline().split()))
+    print(line)
+    for i in range(len(line[0])):
+        if line[0][i] == '?' and line[1][i] == '?':
+            if i + 2 <= len(line[0]):
+                if line[0][i + 1] != '?' and line[1][i + 1] != '?':
+                    if int(line[0][i + 1]) == int(line[1][i + 1]):
+                        line[0][i] = '0'
+                        line[1][i] = '0'
+                    elif abs(int(line[0][i + 1]) - int(line[1][i + 1])) < 5:
+                        line[0][i] = '0'
+                        line[1][i] = '0'
+                        if int(line[0][i + 1]) < int(line[1][i + 1]):
+                            for j in range(i, len(line[0])):
+                                if line[0][j] == '?':
+                                    line[0][j] = '9'
+                                if line[1][j] == '?':
+                                    line[1][j] = '0'
+                            break
+                        else:
+                            for j in range(i, len(line[0])):
+                                if line[0][j] == '?':
+                                    line[0][j] = '0'
+                                if line[1][j] == '?':
+                                    line[1][j] = '9'
+                            break
+                    elif abs(int(line[0][i + 1]) - int(line[1][i + 1])) > 5:
+                        if int(line[0][i + 1]) < int(line[1][i + 1]):
+                            line[0][i] = '1'
+                            line[1][i] = '0'
+                            for j in range(i, len(line[0])):
+                                if line[0][j] == '?':
+                                    line[0][j] = '0'
+                                if line[1][j] == '?':
+                                    line[1][j] = '9'
+                            break
+                        else:
+                            line[0][i] = '0'
+                            line[1][i] = '1'
+                            for j in range(i, len(line[0])):
+                                if line[0][j] == '?':
+                                    line[0][j] = '9'
+                                if line[1][j] == '?':
+                                    line[1][j] = '0'
+                            break
+                    else:
+                        if int(line[0][i + 1]) < int(line[1][i + 1]):
+                            for j in range(i, len(line[0])):
+                                if line[0][j] != '?':
+                                    line[0][j] = '0'
+                                if line[1][j] != '?':
+                                    line[1][j] = '9'
+                            break
+                        else:
+                            for j in range(i, len(line[0])):
+                                if line[0][j] == '?':
+                                    line[0][j] = '9'
+                                if line[1][j] == '?':
+                                    line[1][j] = '0'
+                            break
+            else:
+                line[0][i] = '0'
+                line[1][i] = '0'
+        elif line[0][i] == '?' or line[1][i] == '?':
+            line[0][i] = '0'
+            line[1][i] = '0'
 
-	fout.write('Case #{0}: {1} {2}\n'.format(t, ''.join(line[0]), ''.join(line[1])))
+    fout.write('Case #{0}: {1} {2}\n'.format(
+        t, ''.join(line[0]), ''.join(line[1])))
 
 fin.close()
 fout.close()
